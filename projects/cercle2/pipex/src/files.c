8e4de0dfbf79fec1740a82fe4bcea069d9e54450
Salih Soysal <akaiissa@ordinatedesalih.home>
2025-04-25 14:25:42 +0200

    corrected heredoc

--- projects/cercle2/pipex/src/files.c
+++ projects/cercle2/pipex/src/files.c
@@ -6,7 +6,7 @@
 /*   By: akaiissa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 16:49:58 by akaiissa          #+#    #+#             */
-/*   Updated: 2025/04/25 13:46:17 by akaiissa         ###   ########.fr       */
+/*   Updated: 2025/04/25 14:23:40 by akaiissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,30 +38,58 @@ void file2(t_pipex *p)
     if(p->fdo == -1)
         fail_msg(strerror(errno), ": ", p->av[p->ac - 1], 1);
 }
-
-void hdparse(t_pipex *p)
-{
+void hdparse(t_pipex *p) {
     int tmp;
     int fd_std;
-    char    *buff;
+    char *buff;
 
     tmp = open(".hd.tmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
     fd_std = dup(STDIN_FILENO);
     if (tmp == -1)
         clean_exit(fail_msg("here_doc", ": ", strerror(errno), 1), p);
+
     buff = "";
-    while (1)
-    {
+    while (1) {
         ft_putstr_fd("here_doc > ", 1);
         buff = get_next_line(fd_std);
-        if (buff == NULL)
-            break ;
-        if (ft_strlen(p->av[2]) + 1 == ft_strlen(buff)
-                && !ft_strncmp(buff, p->av[2], ft_strlen(p->av[2] + 1)))
-            close(fd_std);
-        else
-            ft_putstr_fd(buff, tmp);
+        if (buff == NULL) // Gestion de Ctrl+D ou erreur de lecture
+            break;
+        // Correction : parenthèse fixée et comparaison exacte
+        if (ft_strncmp(buff, p->av[2], ft_strlen(p->av[2])) == 0
+            && ft_strlen(buff) == ft_strlen(p->av[2]) + 1) { // +1 pour le '\n'
+            free(buff);
+            break; // Sortie immédiate de la boucle
+        }
+        ft_putstr_fd(buff, tmp); // Écrit dans le fichier temporaire
         free(buff);
     }
     close(tmp);
+    close(fd_std); // Nettoyage
 }
+//
+//void hdparse(t_pipex *p)
+//{
+    //int tmp;
+    //int fd_std;
+    //char    *buff;
+//
+    //tmp = open(".hd.tmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    //fd_std = dup(STDIN_FILENO);
+    //if (tmp == -1)
+        //clean_exit(fail_msg("here_doc", ": ", strerror(errno), 1), p);
+    //buff = "";
+    //while (1)
+    //{
+        //ft_putstr_fd("here_doc > ", 1);
+        //buff = get_next_line(fd_std);
+        //if (buff == NULL)
+            //break ;
+        //if (ft_strlen(p->av[2]) + 1 == ft_strlen(buff)
+                //&& !ft_strncmp(buff, p->av[2], ft_strlen(p->av[2] + 1)))
+            //close(fd_std);
+        //else
+            //ft_putstr_fd(buff, tmp);
+        //free(buff);
+    //}
+    //close(tmp);
+//}
